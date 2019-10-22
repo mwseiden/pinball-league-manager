c3728ac2d56fda16c04d6368f64fd2b373a4dc34
Mark Seiden <mseiden@atlantistech.com>
2019-10-22 14:34:32 -0400

    Add initial models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,19 @@
 class User < ApplicationRecord
-    validates_uniqueness_of :username
-    has_secure_password 
-    has_secure_token :auth_token
-    
-    def invalidate_token 
-        self.update_columns(auth_token: nil)
-    end 
-    
-    def self.validate_login(username, password)
-        user = find_by(username: username)
-        
-        if user && user.authenticate(password)
-            user
-        end
-    
+  has_one :player, inverse_of: :user
+
+  validates_uniqueness_of :username
+  has_secure_password
+  has_secure_token :auth_token
+
+  def invalidate_token
+    self.update_columns(auth_token: nil)
+  end
+
+  def self.validate_login(username, password)
+    user = find_by(username: username)
+
+    if user && user.authenticate(password)
+      user
     end
+  end
 end
